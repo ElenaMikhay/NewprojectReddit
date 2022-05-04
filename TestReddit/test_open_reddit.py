0bf07b4a357b48a9aa9800661a47fa0f43e18504
myback <54638513+myback@users.noreply.github.com>
2022-05-04 23:59:50 +0700

    add authentication for reddit.com

--- TestReddit/test_open_reddit.py
+++ TestReddit/test_open_reddit.py
@@ -1,10 +1,29 @@
 import requests
+import requests.auth
+
 
 class RedditTests:
-    def __init__(self, login, password):
-        self._url = "https://reddit.com/"
+    def __init__(self, login, password, client_id, client_secret):
+        self._url = 'https://reddit.com'
         self._login = login
         self._password = password
+
+        # client_id и client_secret надо сгенерировать на странице https://www.reddit.com/prefs/apps
+        # создав новый dev application для скрипта
+        # "Script for personal use. Will only have access to the developers accounts"
+        self._client_id = client_id
+        self._client_secret = client_secret
+
+        # инициируем новую сессиию, которая будет у нас использована по всему классу
+        self._session = requests.session()
+
+        # как оказалось, в API reddit.com, наличие заголовока User-Agent обязательно.
+        # Почему так хитро добавлен? В именах переменных нельзя использовать "-", это ошибка. А так как headers.update
+        # принимает на вход **kwargs (key=value), а в key у нас есть "-", тогда мы можем воспользоваться распаковковкой
+        # https://docs.python.org/3/tutorial/controlflow.html#unpacking-argument-lists
+        # https://stackoverflow.com/a/5710402
+        # Тогда при каждом запросе, у нас автоматически данный заголовок будет подставлятся в запрос
+        self._session.headers.update(**{"User-Agent": "TestForJob/0.0.1"})
 
     def check_availability_reddit(self):
         """Открытие сайта Reddit"""
@@ -12,81 +31,33 @@ class RedditTests:
         resp = requests.get(self._url)
         resp.raise_for_status()
         if resp.status_code == 200:
+            # А если статус 301, тогда метод вернет None и мы получим ошибку если что-то попытаемся извлечь из ответа.
+            # Для проверки доступности самого сайта думаю хватит raise_for_status, если ошибка не выскочила, то значит
+            # работает
             return resp.status_code
 
     def auth(self):
         """Аутентификация на сайте"""
 
-        session = requests.session()
-        url = self._url+'login'
-        params = {
-            'csrf_token': 'ac0a1b8d7a4490a9103282269372ae6784fce79d',
-            'otp': '',
-            'password': '!QAZ2wsx',
-            'dest': f'{self._url}',
-            'username': 'TestForJob'
-        }
-        r = session.post(url, params)
-        print(r.text)
+        # создаем объекты для аутентификации на reddit.com
+        auth_hdr = requests.auth.HTTPBasicAuth(self._client_id, self._client_secret)
+        data = {'grant_type': 'password', 'username': self._login, 'password': self._password}
 
-               #
-        #    # url https://www.reddit.com/login
-        #    # POST
-        #    # Headers:
-        #    # Body:csrf_token=ac0a1b8d7a4490a9103282269372ae6784fce79d&otp=&password=!QAZ2wsx&dest=https%3A%2F%2Fwww.reddit.com&username=TestForJob
-        #    # content-type: application/x-www-form-urlencoded
-        #    # accept:*/*
-        #    # accept-encoding:gzip, deflate, br
-        #    # accept-language: ru,en;q=0.9
-        #    #На выходе получить токен
-        #    #login: TestForJob Password: !QAZ2wsx
-        #
-        #    def search_thread(self):
-        #        """Поиск треда по ключевым словам"""
-        #
-        #       url: https: // oauth.reddit.com / api / subreddit_autocomplete_v2.json?query = museum & raw_json = 1 & gilding_detail = 1
-        #
-        #
-        #
-        #
-        #    def add_comment(self):
-        #        """Добавление комментария в тред"""
-        #        https: // oauth.reddit.com / api / comment.json?rtj = only & emotes_as_images = true & redditWebClient = desktop2x & app = desktop2x - client - production & raw_json = 1 & gilding_detail = 1
-        ## accept:*/*
-        ## accept-encoding:gzip, deflate, br
-        ## accept-language: ru,en;q=0.9
-        ##POST
-        ##Headers: authorization: Bearer...из метода authtorization
-        ##content-type:application/x-www-form-urlencoded
-        ##Body: api_type=json&return_rtjson=true&thing_id=t3_u4nxjk&text&richtext_json=%7B%22document%22%3A%5B%7B%22e%22%3A%22par%22%2C%22c%22%3A%5B%7B%22e%22%3A%22text%22%2C%22t%22%3A%22fgdfgd%22%7D%5D%7D%5D%7D
-        ##
-        ##Зафиксировать name
-        #
-        #
-        #    def delete_comment(self):
-        #        """Удаление ранее оставленного комментария"""
-        ##https://oauth.reddit.com/api/del?redditWebClient=desktop2x&app=desktop2x-client-production&raw_json=1&gilding_detail=1
-        ## accept:*/*
-        ## accept-encoding:gzip, deflate, br
-        ## accept-language: ru,en;q=0.9
-        ##POST
-        ##Headers: authorization: Bearer...из метода authtorization
-        ##content-type:application/x-www-form-urlencoded
-        ##Body: id=t1_i75mgb4     id=add_comment.name
-        #
-        #
-        #    def log_out_of_the_user(self):
-        #        """Выйти из авторизованного пользователя"""
-        #      https://www.reddit.com/logoutproxy
-        #    # POST:
-        #    # accept:*/*
-        #    # accept-encoding:gzip, deflate, br
-        #    # accept-language: ru,en;q=0.9
-        #    # POST
-        #    # Headers: authorization: Bearer...из метода authtorization
-        #    # content-type:application/x-www-form-urlencoded
-        #    #Body:  access_token = 1736311588263 - UbABTB48P2bWPyF6xrphWB7b1sf7Nw ????
+        # запрашиваем токен, он понадобится для работы с API
+        resp = self._session.post(f'{self._url}/api/v1/access_token', auth=auth_hdr, data=data)
+        # проверяем возвращаемый статус от reddit.com, если часто запрашивать токен, то вернется статус 429
+        resp.raise_for_status()
+
+        # у нас всё работает, тогда декодируем json в словарь (dict())
+        resp_data = resp.json()
+
+        # добавляем в заголовки нашей сессии, заголовок Authorization чтобы не добавлять это в каждый наш запрос
+        self._session.headers.update(Authorization=f"{resp_data['token_type']} {resp_data['access_token']}")
+
+        # после того, как получили токен и добавили его в заголовки можем перейти к API-запросам
+
 
 if __name__ == '__main__':
-    rt = RedditTests("TestForJob", "!qaz2wsx")
+    rt = RedditTests("TestForJob", "!qaz2wsx", "client_id", "client_secret")
     rt.auth()
+# всегда в конце файла должна быть пустая строка
